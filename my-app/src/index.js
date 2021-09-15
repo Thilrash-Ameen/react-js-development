d5f44c09828e15356f9e94560da6aa21e08ef138
Thilrash-Ameen <thilrashameen@gmail.com>
2021-09-15 16:42:32 +0530

    jsx-css commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-// arrow function 
-// const Greeting = () => {
-//   // return <h1>Hello World</h1>;
-//   return React.createElement('div', {}, React.createElement('h1', {}, 'Hello World'));
-// }
+import './index.css';
 
-// traditional function method
-function Greeting() {
+const BookList = () => {
   return(
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
+const Book = () => {
+  return <article className='book'>
+    <Image />
+    <Title />
+    <Author />
+  </article>;
+}
+
+const Image = () => {
+  return <img src="https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg" alt="" width="30%"></img>
+}
+
+const Title = () => {
+  return <h1>I Love You to the Moon and Back</h1>
+}
+
+const Author = () => {
+  return <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Jackson Deep Mila</h4>
+}
+
 // converting the react component as a HTML element
-ReactDom.render(<Greeting />, document.getElementById('root'));
+ReactDom.render(<BookList />, document.getElementById('root'));
