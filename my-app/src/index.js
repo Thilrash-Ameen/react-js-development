e0172e5e72a42425961c6812590442829420a7a3
Thilrash-Ameen <thilrashameen@gmail.com>
2021-09-18 13:51:50 +0530

    props-with-children commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,23 +3,37 @@ import ReactDom from 'react-dom';
 
 import './index.css';
 
-const title = 'I Love You to the Moon and Back';
-const author = 'Jackson Deep Mila';
-const image = "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg";
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/811SRapU9aL._AC_UL200_SR200,200_.jpg",
+  title: 'School Zone - Big Preschool Workbook',
+  author: 'Paperback'
+}
 
-const BookList = () => {
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg",
+  title: 'I Love You to the Moon and Back',
+  author: 'ackson Deep Mila'
+}
+
+function BookList() {
   return(
     <section className='booklist'>
-      <Book />
+      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna erat, rhoncus non suscipit quis, suscipit sit amet nulla.</p>
+      </Book>
+      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} /> 
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  const {img, title, author, children} = props;
+  console.log(props);
   return <article className='book'>
-    <img src={image} alt='' width="30%"/>
+    <img src={img} alt='' width="30%"/>
     <h1>{title}</h1>
-    <h4>{author.toUpperCase()}</h4>
+    <h4>{author}</h4>
+    {children}
   </article>;
 }
 
